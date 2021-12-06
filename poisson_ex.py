d3dea92053abf66464762cb10d82d13be7586b2d
CamiloFajardo2021 <92040860+CamiloFajardo2021@users.noreply.github.com>
2021-12-05 21:06:42 -0500

    Update poisson_ex.py

--- poisson_ex.py
+++ poisson_ex.py
@@ -1,24 +1,24 @@
-from dolfin import *
+import fenics as fe
 # Create mesh and define function space
-mesh = UnitSquare(6, 4)
-V = FunctionSpace(mesh, "Lagrange", 1)
+mesh = fe.UnitSquare(6, 4)
+V = fe.FunctionSpace(mesh, "Lagrange", 1)
 # Define boundary conditions
-u0 = Expression("1 + x[0]*x[0] + 2*x[1]*x[1]")
+u0 = fe.Expression("1 + x[0]*x[0] + 2*x[1]*x[1]")
 def u0_boundary(x, on_boundary):
     return on_boundary
-bc = DirichletBC(V, u0, u0_boundary)
+bc = fe.DirichletBC(V, u0, u0_boundary)
 # Define variational problem
-u = TrialFunction(V)
-v = TestFunction(V)
-f = Constant(-6.0)
-a = inner(nabla_grad(u), nabla_grad(v))*dx
+u = fe.TrialFunction(V)
+v = fe.TestFunction(V)
+f = fe.Constant(-6.0)
+a = fe.inner(nabla_grad(u), nabla_grad(v))*dx
 L = f*v*dx
 # Compute solution
-u = Function(V)
-solve(a == L, u, bc)
+u = fe.Function(V)
+fe.solve(a == L, u, bc)
 # Plot solution and mesh
-plot(u)
-plot(mesh)
+fe.plot(u)
+fe.plot(mesh)
 # Dump solution to file in VTK format
 file = File("poisson.pvd")
 file << u
