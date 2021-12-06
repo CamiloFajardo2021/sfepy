8a564638807c78f31fe9b92fd55c14827770e422
CamiloFajardo2021 <92040860+CamiloFajardo2021@users.noreply.github.com>
2021-12-05 21:25:43 -0500

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -30,7 +30,7 @@ def epsilon(u):
     #return sym(nabla_grad(u))
 
 def sigma(u):
-    return lambda_*nabla_div(u)*Identity(d) + 2*mu*epsilon(u)
+    return lambda_ * nabla_div(u)*Identity(d) + 2*mu*epsilon(u)
 
 # Define variational problem
 u = TrialFunction(V)
